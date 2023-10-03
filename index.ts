f13836d829581fdd603f0dad953ff07195bc7419
OSmall <oliver.small232@gmail.com>
2023-10-03 11:56:20 +1100

    options object

--- index.ts
+++ index.ts
@@ -10,3 +10,16 @@ let p: Partial<Person> = {
 
 console.log(p);
 console.log("Hello via Bun!");
+
+
+/** 
+ * Testing options object design pattern
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters#destructured_parameter_with_default_value_assignment}
+*/
+function foo(id: string, { displayName = id } = {}) {
+    displayName = displayName.replace('or', 'i');
+    console.log(displayName);
+}
+
+foo('hello');
+foo('hello', { displayName: 'world' });
